86d820c1767413c7256767e8f0678d4be714d442
ygs <yugyusang12@gmail.com>
2026-06-10 15:20:25 +0900

    feat: 달리는 텍스쳐 등록

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,7 @@
-import { CSSProperties, useEffect, useRef, useState } from "react";
+import { CSSProperties, useCallback, useEffect, useRef, useState } from "react";
 import { Application, extend } from "@pixi/react";
-import spriteImage from "./assets/Cat-1-Walk.png"; //https://luizmelo.itch.io/pet-cat-pack
+import walkImage from "./assets/Cat-1-Walk.png"; //https://luizmelo.itch.io/pet-cat-pack
+import runImage from "./assets/Cat-1-Run.png"; //https://luizmelo.itch.io/pet-cat-pack
 import { Assets, Container, Graphics, Rectangle, Sprite, Texture, AnimatedSprite } from "pixi.js";
 import { listen } from "@tauri-apps/api/event";
 extend({
@@ -14,31 +15,45 @@ const FRAME_WIDTH = 50;
 const FRAME_HEIGHT = 50;
 const TOTAL_FRAMES = 8;
 
-listen("global-input", (event) => {
-  console.log(event);
-});
-
 const Home = () => {
   const parentRef = useRef<HTMLDivElement>(null);
-  const [catFrames, setCatFrames] = useState<Texture[]>([]);
+  const [isRunning, setIsRunning] = useState(false);
+  const [walkFrames, setWalkFrames] = useState<Texture[]>([]);
+  const [runFrames, setRunFrames] = useState<Texture[]>([]);
+
+  listen("global-input", (event) => {
+    console.log(event);
+    setIsRunning(true);
+  });
 
   useEffect(() => {
-    async function loadCatTextures() {
-      const baseTexture = await Assets.load<Texture>(spriteImage);
-      baseTexture.source.scaleMode = "nearest"; // 도트 감성 유지
+    async function loadAllTextures() {
+      const walkBaseTexture = await Assets.load<Texture>(walkImage);
+      walkBaseTexture.source.scaleMode = "nearest"; // 도트 감성 유지
+      const runBaseTexture = await Assets.load<Texture>(runImage);
+      runBaseTexture.source.scaleMode = "nearest";
 
-      const frames: Texture[] = [];
+      const walkFrames: Texture[] = [];
+      const runFrames: Texture[] = [];
       for (let i = 0; i < TOTAL_FRAMES; i++) {
-        frames.push(
+        walkFrames.push(
           new Texture({
-            source: baseTexture.source,
+            source: walkBaseTexture.source,
+            frame: new Rectangle(i * FRAME_WIDTH, 0, FRAME_WIDTH, FRAME_HEIGHT),
+          }),
+        );
+
+        runFrames.push(
+          new Texture({
+            source: runBaseTexture.source,
             frame: new Rectangle(i * FRAME_WIDTH, 0, FRAME_WIDTH, FRAME_HEIGHT),
           }),
         );
       }
-      setCatFrames(frames);
+      setWalkFrames(walkFrames);
+      setRunFrames(runFrames);
     }
-    loadCatTextures();
+    loadAllTextures();
   }, []);
 
   const spriteRefCallback = (instance: AnimatedSprite) => {
@@ -47,15 +62,14 @@ const Home = () => {
     }
   };
 
-  if (catFrames.length === 0) return null;
   return (
     <div ref={parentRef} style={{ width: "100vw", height: "100vh" }}>
       <Application resizeTo={parentRef} backgroundAlpha={0}>
         <pixiContainer x={50} y={50}>
           <pixiAnimatedSprite
             autoPlay={true}
-            textures={catFrames}
-            animationSpeed={0.15} // 재생 속도
+            textures={isRunning ? runFrames : walkFrames}
+            animationSpeed={isRunning ? 0.15 : 0.3} // 재생 속도
             anchor={0.5}
             ref={spriteRefCallback}
           />
